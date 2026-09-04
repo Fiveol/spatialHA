98904a88adc0bf74995dc27751de64779c76d124
Fiveol <andyinventor@outlook.com>
2026-09-03 20:15:17 -0700

    Initial commit

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,8 +1,7 @@
-"""Constants for integration_blueprint."""
+"""Constants for spatialHA."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+DOMAIN = "spatialHA"
